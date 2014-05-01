759d403efa169e6dec40f1a53b69f4b3a1246199
Matt Lummus <matthew.w.lummus@gmail.com>
2014-05-01 12:50:10 -0500

    issue with link interpolation fixed

--- giffer.rb
+++ giffer.rb
@@ -7,26 +7,30 @@ class App
 
     #Open a database
     db = SQLite3::Database.new('gifs.db')
-    db.execute "CREATE TABLE IF NOT EXISTS Gifs(id INTEGER PRIMARY KEY,
-        url TEXT, category TEXT, emotion TEXT, reference TEXT)"
+    #db.execute "CREATE TABLE IF NOT EXISTS Gifs(id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, category TEXT, emotion TEXT, reference TEXT)"
+    db.execute "CREATE TABLE IF NOT EXISTS Gifs(url TEXT, category TEXT, emotion TEXT, reference TEXT)"
 
     if input[0] == "give"
       puts "give a gif"
-      id = (db.last_insert_row_id + 1)# || 1
       url = input[1].dup
       category = input[2]
       emotion = input[3]
       reference = input[4]
-      #db.execute "INSERT INTO Gifs VALUES (#{id}, #{url}, #{category}, #{emotion}, #{reference})"
-      puts "INSERT INTO Gifs VALUES (#{id}, #{url}, #{category}, #{emotion}, #{reference})"
+      db.execute "INSERT INTO Gifs VALUES('#{url}', '#{category}', '#{emotion}', '#{reference}')"
 
     elsif input[0] == "get"
-      puts "give a gif"
+      puts "get a gif"
       link = 'https://i.imgur.com/LsglmGb.gif'
-      IO.popen('open -a Google\ Chrome '+link, 'r+') do |pipe|
-        pipe.close_write
-      end #of IO
-
+      stm = db.prepare "SELECT url FROM Gifs LIMIT 5"
+      rs = stm.execute
+      rs.each do |row|
+        row = row.to_s
+        row.gsub!("[", '')
+        row.gsub!("]", '')
+        IO.popen('open -a Google\ Chrome '+row, 'r+') do |pipe|
+          pipe.close_write
+        end #of IO
+      end
     else
       puts "There was an error in your request."
       puts "Try one of these requests:"
